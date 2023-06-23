2a6c5077861f7c84d7071303f9a3ad4ce991ed14
SSH <bko2369@naver.com>
2023-06-23 09:37:43 +0900

    [2023.6.23] 9:36 서수현 /

--- GC25 Project/src/main/java/com/gc25/controllers/MemberController.java
+++ GC25 Project/src/main/java/com/gc25/controllers/MemberController.java
@@ -378,8 +378,6 @@ public class MemberController extends HttpServlet {
 					}
 					if(request.getParameter("memberPwd")!=null){
 						memberPwd=request.getParameter("memberPwd");
-						
-						
 					}
 					if(request.getParameter("memberNickname")!=null){
 						memberNickname=request.getParameter("memberNickname");
@@ -479,10 +477,7 @@ public class MemberController extends HttpServlet {
 					
 					PrintWriter out = response.getWriter();
 					String passwordReg = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@!%*#?&])[A-Za-z\\d@!%*#?&]{8,12}$";
-<<<<<<< HEAD
-=======
-				
->>>>>>> branch 'master' of https://github.com/Not-Potato/GC25.git
+
 					memberEmail=(String)session.getAttribute("memberEmail");
 					
 					//로그인 정보 확인
@@ -586,7 +581,7 @@ public class MemberController extends HttpServlet {
 					
 					//memberService.getMemberImageFileName(memberImageFileName, memberEmail);
 					//파일 저장할 경로
-					File curPath=new File("C:\\Users\\HJ LEE\\git\\GC25\\GC25 Project\\src\\main\\webapp\\resources\\images\\profileimages\\");
+					File curPath=new File("C:\\Users\\bko23\\git\\GC25\\GC25 Project\\src\\main\\webapp\\resources\\images\\profileimages\\");
 //					System.out.println(curPath);
 					//DiskFileItemFactory는 FileItem 객체를 생성하기 위한 팩토리 클래스  
 					//->getSize(), getName(), isFormField 등을 제공 -> 업로드된 파일의 정보(이름, 크기, 타입 등)를 확인하고, 데이터에 접근하여 원하는 처리를 수행 
